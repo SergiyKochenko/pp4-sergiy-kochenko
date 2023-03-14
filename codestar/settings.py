e374f08ec775c993614f631da2edba65fac4a0b4
SergiyKochenko <sergiykochenko@gmail.com>
2023-03-14 00:15:26 +0000

    first deployment commit

--- codestar/settings.py
+++ codestar/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://pp4-sergiy-kochenko.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['pp4-sergiy-kochenko.herokuapp.com', 'localhost']
 
 
 # Application definition
